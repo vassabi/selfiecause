658aee0bb05792469c0b40f4e0724a1d6004bf3e
Juju2993 <sue.keshishyan@gmail.com>
2017-12-12 16:22:34 +0400

    Camera files 2

--- www/js/controllers/camera.js
+++ www/js/controllers/camera.js
@@ -1,3 +1,41 @@
 ﻿$(document).ready(function () {
-
+    $("#btn-take-picture").on("click", function () {
+        openCamera(selection);
+    });
 });
+
+function openCamera(selection) {
+    var srcType = Camera.PictureSourceType.CAMERA;
+    var options = setOptions(srcType);
+
+    navigator.camera.getPicture(function cameraSuccess(imageUri) {
+
+        displayImage(imageUri);
+        // You may choose to copy the picture, save it somewhere, or upload.
+
+    }, function cameraError(error) {
+        console.debug("Unable to obtain picture: " + error, "app");
+
+    }, options);
+}
+
+function displayImage(imgUri) {
+    var img = $("img");
+    img.prop("src", imgUri);
+    $("#image-holder").empty().append(img);
+}
+
+function setOptions(srcType) {
+    var options = {
+        // Some common settings are 20, 50, and 100
+        quality: 50,
+        destinationType: Camera.DestinationType.FILE_URI,
+        // In this app, dynamically set the picture source, Camera or photo gallery
+        sourceType: srcType,
+        encodingType: Camera.EncodingType.JPEG,
+        mediaType: Camera.MediaType.PICTURE,
+        allowEdit: true,
+        correctOrientation: true  //Corrects Android orientation quirks
+    }
+    return options;
+}
