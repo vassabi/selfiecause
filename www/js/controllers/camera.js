235b56cd9b6c2b66902f84c7f9bdeb4f62f53665
Juju2993 <sue.keshishyan@gmail.com>
2017-12-12 16:43:04 +0400

    Camera files 3

--- www/js/controllers/camera.js
+++ www/js/controllers/camera.js
@@ -1,41 +1,29 @@
 ﻿$(document).ready(function () {
+    let options = {
+        x: 0,
+        y: 0,
+        width: window.screen.width,
+        height: window.screen.height,
+        camera: CameraPreview.CAMERA_DIRECTION.BACK,
+        toBack: false,
+        tapPhoto: true,
+        previewDrag: false
+    };
+
+    CameraPreview.startCamera(options);
+
     $("#btn-take-picture").on("click", function () {
-        openCamera(selection);
+        CameraPreview.takePicture({ width: 640, height: 640, quality: 85 }, function (base64PictureData) {
+            /*
+              base64PictureData is base64 encoded jpeg image. Use this data to store to a file or upload.
+              Its up to the you to figure out the best way to save it to disk or whatever for your application.
+            */
+
+            // One simple example is if you are going to use it inside an HTML img src attribute then you would do the following: 
+            imageSrcData = 'data:image/jpeg;base64,' + base64PictureData;
+            $('img#my-img').attr('src', imageSrcData);
+            $("#image-holder").empty().append($('img#my-img'));
+        });
     });
 });
 
-function openCamera(selection) {
-    var srcType = Camera.PictureSourceType.CAMERA;
-    var options = setOptions(srcType);
-
-    navigator.camera.getPicture(function cameraSuccess(imageUri) {
-
-        displayImage(imageUri);
-        // You may choose to copy the picture, save it somewhere, or upload.
-
-    }, function cameraError(error) {
-        console.debug("Unable to obtain picture: " + error, "app");
-
-    }, options);
-}
-
-function displayImage(imgUri) {
-    var img = $("img");
-    img.prop("src", imgUri);
-    $("#image-holder").empty().append(img);
-}
-
-function setOptions(srcType) {
-    var options = {
-        // Some common settings are 20, 50, and 100
-        quality: 50,
-        destinationType: Camera.DestinationType.FILE_URI,
-        // In this app, dynamically set the picture source, Camera or photo gallery
-        sourceType: srcType,
-        encodingType: Camera.EncodingType.JPEG,
-        mediaType: Camera.MediaType.PICTURE,
-        allowEdit: true,
-        correctOrientation: true  //Corrects Android orientation quirks
-    }
-    return options;
-}
