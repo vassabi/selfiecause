520cb2ff980a31702854194cf545cbe1bc00c3af
Juju2993 <sue.keshishyan@gmail.com>
2017-12-13 18:29:59 +0400

    Camera flash

--- www/js/controllers/camera.js
+++ www/js/controllers/camera.js
@@ -16,8 +16,23 @@
         CameraPreview.switchCamera();
     });
 
+    $("#btn-flash").on("click", function () {
+        CameraPreview.getFlashMode(function (currentFlashMode) {
+
+            if (currentFlashMode == CameraPreview.FLASH_MODE.OFF)
+                CameraPreview.setFlashMode(CameraPreview.FLASH_MODE.ON);
+            else
+                CameraPreview.setFlashMode(CameraPreview.FLASH_MODE.OFF);
+        });
+    });
+
+    $("#btn-close").on("click", function () {
+        $('img#my-img').attr('src', '');
+        $('img#my-img').attr('style', 'display:none;');
+    });
+
     $("#btn-take-picture").on("click", function () {
-        CameraPreview.takePicture({ width: 640, height: 640, quality: 85 }, function (base64PictureData) {
+        CameraPreview.takePicture({ quality: 85 }, function (base64PictureData) {
             imageSrcData = 'data:image/jpeg;base64,' + base64PictureData;
             $('img#my-img').attr('src', imageSrcData);
             $('img#my-img').attr('style', 'display:block;');
