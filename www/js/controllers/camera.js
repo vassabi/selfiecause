a6996bb8261694682aeaea0c1062e176230059fb
Juju2993 <sue.keshishyan@gmail.com>
2017-12-13 18:07:49 +0400

    Camera take picture functionality

--- www/js/controllers/camera.js
+++ www/js/controllers/camera.js
@@ -9,21 +9,16 @@
         tapPhoto: true,
         previewDrag: false
     };
-    
+
+    CameraPreview.startCamera(options);
 
     $("#btn-take-picture").on("click", function () {
-        CameraPreview.startCamera(options);
-        //CameraPreview.takePicture({ width: 640, height: 640, quality: 85 }, function (base64PictureData) {
-        //    /*
-        //      base64PictureData is base64 encoded jpeg image. Use this data to store to a file or upload.
-        //      Its up to the you to figure out the best way to save it to disk or whatever for your application.
-        //    */
-
-        //    // One simple example is if you are going to use it inside an HTML img src attribute then you would do the following: 
-        //    imageSrcData = 'data:image/jpeg;base64,' + base64PictureData;
-        //    $('img#my-img').attr('src', imageSrcData);
-        //    $("#image-holder").empty().append($('img#my-img'));
-        //});
+        CameraPreview.takePicture({ width: 640, height: 640, quality: 85 }, function (base64PictureData) {
+            imageSrcData = 'data:image/jpeg;base64,' + base64PictureData;
+            var img = $('img');
+            img.attr('src', imageSrcData);
+            $("#image-holder").empty().append(img);
+        });
     });
 });
 
