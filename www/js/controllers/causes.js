0d9e91a108e3c48b8ac77625fbaa7a36f0fc906d
Mayank Singh <mysingh@synapseco.com>
2018-01-17 12:20:06 +0530

    Menu is working on causes screen and removed the loader from botton after causes datas gets loaded

--- www/js/controllers/causes.js
+++ www/js/controllers/causes.js
@@ -1,22 +1,26 @@
 $(document).ready(function () {
     InjectView("loader", "loader");
     InjectView("slidemenu", "header");
+    
     var dataSource = new kendo.data.DataSource({
         transport: {
             read: {
                 url: apiUrl + "?action=causes",
-                dataType: "json"
+                dataType: "json",
             }
         }
     });
-    $("#listView").kendoListView({
+  
+                $("#listView").kendoListView({
         dataSource: dataSource,
         template: kendo.template($("#template").html())
     });
-
-    debugger;
-    EmptyView("loader");
-    setTitle("Causes");
-    initCustomForms();
-    initMobileNav();
+    
+setTimeout(function(){
+      setTitle("Causes");
+     initCustomForms();
+      initMobileNav();
+      EmptyView("loader");  
+  },300)
+   
 });
