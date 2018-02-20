df89bd8d82677b3a162d34583d770e3ee92b678b
Juju2993 <sue.keshishyan@gmail.com>
2018-02-20 13:22:21 +0400

    camera changes

--- www/js/controllers/picture_edit_reframe.js
+++ www/js/controllers/picture_edit_reframe.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function () {
     var data = mobile.passedData
-    $("#img").src = data.base64PictureData;
+    $("#img").attr("src", data.base64PictureData);
     $("#btn_back").on("click", function () {
         LoadView("camera", null, data.campaignid, "down");
     });
