6b45865577a32f65f45f7148137c4f98e8c3ebb3
Mayank <asashutosh@synapseindia.email>
2018-02-22 13:40:50 +0530

    changes

--- www/js/controllers/picture_edit_reframe.js
+++ www/js/controllers/picture_edit_reframe.js
@@ -1,5 +1,8 @@
 ﻿$(document).ready(function () {
     $("#img").attr("src", mobile.passedData.base64PictureData);
+
+	
+	
     $("#btn_back").on("click", function () {
         LoadView("camera", null, data.campaignid, "down");
     });
@@ -7,50 +10,78 @@
     $("#btn_save").on("click", function () {
         SpinnerPlugin.activityStart("Saving...", options);
         var data = { action: "awscreds" };
-        callApi(data, "GET", function success(d) {
-        //    alert('api call success ' + d.accessKeyId);
+    
+    callApi(data, "GET", function success(d) {
+      //     alert('api call success ' + d.accessKeyId);
           
                  
- var dataURI = mobile.passedData.base64PictureData.replace(/^data:/, '');
+var c = document.createElement('canvas');
+var ctx = c.getContext('2d');
+var img = document.getElementById('img');
+ctx.drawImage(img, 10, 10);
+var base64String = c.toDataURL();
 
-    const type = mobile.passedData.base64PictureData.match(/image\/[^;]+/);
-    const base64 = mobile.passedData.base64PictureData.replace(/^[^,]+,/, '');
-    const arrayBuffer = new ArrayBuffer(base64.length);
-    const typedArray = new Uint8Array(arrayBuffer);
-
-    for (let i = 0; i < base64.length; i++) {
-        typedArray[i] = base64.charCodeAt(i);
-    }
-
-    var blob= new Blob([arrayBuffer], {type});
+//alert(base64String);
 
 
-			//	 alert('b64toBlob ' + blob);
-                    var url = window.URL.createObjectURL(blob);
-                    var xhr = new XMLHttpRequest();
-                    xhr.open("GET", url, true);
-                    xhr.responseType = "arraybuffer";
-                    xhr.onload = function (ev) {
-                        // Obtain a blob: URL for the image data.
-                        var arrayBufferView = new Uint8Array(this.response);
-                        var blob = new Blob([arrayBufferView], { type: "image/jpeg" });
-                    };
-                    xhr.send();
+    // const type = mobile.passedData.base64PictureData.match(/image\/[^;]+/);
+    // const base64 = mobile.passedData.base64PictureData.replace(/^[^,]+,/, '');
+    // const arrayBuffer = new ArrayBuffer(base64.length);
+    // const typedArray = new Uint8Array(arrayBuffer);
 
-                    uploadToS3(blob, d, function (err, data) {
-               //         alert('uploadToS3' + blob);
-                        if (data) {
-                        
-						SpinnerPlugin.activityStop();
-							alert('Image Uploaded');
+    // for (let i = 0; i < base64.length; i++) {
+        // typedArray[i] = base64.charCodeAt(i);
+    // }
+	
+b64toBlob(base64String,
+  
+   function(blob) {
+        
+       var url = window.URL.createObjectURL(blob);
+
+     console.log(url);
+     var xhr = new XMLHttpRequest();
+   xhr.open( "GET", url, true );
+   xhr.responseType = "arraybuffer";
+   xhr.onload = function( ev ) {
+       // Obtain a blob: URL for the image data.
+       var arrayBufferView = new Uint8Array( this.response );
+       var blob = new Blob( [ arrayBufferView ], { type: "image/jpeg" } );
+      
+   };
+   
+   xhr.send();
+
+ 
+  uploadToS3(blob,d, function (err, data) {
+     
+	 if (data) {
+           console.log('yay!');
+		   SpinnerPlugin.activityStop();
+							 alert('Image Uploaded');
                             showNotification("Image saved successfully");
-                        }
-                        else {
-                            SpinnerPlugin.activityStop();
-                             alert('not Uploaded');
+       }
+       else{
+           console.log('not successful');
+    
+                    SpinnerPlugin.activityStop();
+                              alert('not Uploaded');
                             showNotification("Unable to save image");
-                        }
-                    });
+
+
+
+	}
+   });
+
+       // do something with url
+   }, function(error) {
+       // handle error
+	   
+	    console.log('n');
+	   
+	   
+	    });
+	
            
         }, function error(d) {
             SpinnerPlugin.activityStop();
@@ -81,6 +112,27 @@ function uploadToS3(blob, awscreds, callback) {
     let s3 = new AWS.S3();
     var d = new Date();
     var t = d.getTime();
-    let options = { Bucket: 'selfiecausebucket', Key: 'myFile' + t + '.jpg', Body: blob };// <--
+    let options = {Bucket: 'selfiecausebucket', Key: 'myFile' + t + '.jpg', Body: blob };// <--
     s3.upload(options, callback);
 }
+
+function b64toBlob(b64, onsuccess, onerror) {
+    var img = new Image();
+
+    img.onerror = onerror;
+
+    img.onload = function onload() {
+        var canvas = document.createElement('canvas');
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        var ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+        canvas.toBlob(onsuccess);
+    };
+
+    img.src = b64;
+}
+
+
